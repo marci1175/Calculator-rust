ff5fb7625bd10e7af6dcafa68197bdd6fb9f98fa
Marcell Varga <v.marcell4000@gmail.com>
2023-11-03 08:45:22 +0100

    kész

--- src/main.rs
+++ src/main.rs
@@ -35,19 +35,27 @@ impl Engine {
 
     fn invalid_equation(&self, err : Error, item_at_fault : String) {
 
-        dbg!(self.expr_list.clone());
-        dbg!(self.num_list.clone());
-
         println!("[{err}]\n{}\n{}", format!("Item(s) at fault => {} [at index : {}]", item_at_fault, *self.buf.iter().position(|element| *element == item_at_fault).get_or_insert(0) + 1 as usize), self.buf.iter().map(|f| format!("{} ", f)).collect::<String>());
-        loop{}
+        std::io::stdin();
+
+        Engine::mathmain(self.clone());
     }
 
     fn mathmain(mut self) -> f64 {
+        self.expr_list.clear();
+        self.num_list.clear();
+
         let mut buf = String::default();
         std::io::stdin().read_line(&mut buf).unwrap();
 
         self.buf = buf.trim().split_whitespace().map(|f| f.to_string()).collect();
-        self.mathdivider()
+        if !self.buf.is_empty() {
+            return self.mathdivider()
+        }
+        else {
+            self.invalid_equation(Error::msg("Invalid equation (Empty equation)"), "-".into());
+            0.0
+        }
     }
     
     fn mathdivider(mut self) -> f64 {
@@ -77,7 +85,6 @@ impl Engine {
     }
     // 30 + 30 * 30
     fn mathengine(&mut self) -> f64 {
-        let mut expr_list_copy = self.expr_list.clone();
         let mut len: usize = self.expr_list.len();
         let mut index = 0;
 
@@ -89,7 +96,37 @@ impl Engine {
                 self.num_list.remove(index);
                 self.num_list.insert(index, result);
                 len -= 1; // Decrement the length since we removed an element
-            } else {
+            
+            } else if self.expr_list[index] == Math::Divide {
+                let result = self.divide(self.num_list[index], self.num_list[index + 1]);
+                self.expr_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.insert(index, result);
+                len -= 1; // Decrement the length since we removed an element
+            } else if self.expr_list[index] == Math::Power {
+                let result = self.power(self.num_list[index], self.num_list[index + 1]);
+                self.expr_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.insert(index, result);
+                len -= 1; // Decrement the length since we removed an element
+            } else if self.expr_list[index] == Math::Addition {
+                let result = self.addition(self.num_list[index], self.num_list[index + 1]);
+                self.expr_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.insert(index, result);
+                len -= 1; // Decrement the length since we removed an element
+            } else if self.expr_list[index] == Math::Subtraction {
+                let result = self.subtraction(self.num_list[index], self.num_list[index + 1]);
+                self.expr_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.remove(index);
+                self.num_list.insert(index, result);
+                len -= 1; // Decrement the length since we removed an element
+            }
+            else {
                 index += 1;
             }
         }
