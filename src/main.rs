55d2e1547740fbde9f2930c4d8a6b3732b226a6d
Marcell Varga <v.marcell4000@gmail.com>
2023-11-03 08:54:35 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ enum Math {
     Divide,
     Addition,
     Subtraction,
-    // ^ 
+    // ^
     Power,
 
     //Empty
-    KurvaAnyad
+    KurvaAnyad,
 }
 
 #[derive(Clone)]
@@ -32,13 +32,27 @@ impl Default for Engine {
 }
 
 impl Engine {
-
-    fn invalid_equation(&self, err : Error, item_at_fault : String) {
-
+    fn invalid_equation(&self, err: Error, item_at_fault: String) {
         dbg!(self.num_list.clone());
         dbg!(self.expr_list.clone());
 
-        println!("[{err}]\n{}\n{}", format!("Item(s) at fault => {} [at index : {}]", item_at_fault, *self.buf.iter().position(|element| *element == item_at_fault).get_or_insert(0) + 1 as usize), self.buf.iter().map(|f| format!("{} ", f)).collect::<String>());
+        println!(
+            "[{err}]\n{}\n{}",
+            format!(
+                "Item(s) at fault => {} [at index : {}]",
+                item_at_fault,
+                *self
+                    .buf
+                    .iter()
+                    .position(|element| *element == item_at_fault)
+                    .get_or_insert(0)
+                    + 1 as usize
+            ),
+            self.buf
+                .iter()
+                .map(|f| format!("{} ", f))
+                .collect::<String>()
+        );
         std::io::stdin();
 
         Engine::mathmain(self.clone());
@@ -49,33 +63,41 @@ impl Engine {
         self.num_list.clear();
 
         let mut buf = String::default();
-        
+
         std::io::stdin().read_line(&mut buf).unwrap();
 
-        self.buf = buf.trim().split_whitespace().map(|f| f.to_string()).collect();
+        self.buf = buf
+            .trim()
+            .split_whitespace()
+            .map(|f| f.to_string())
+            .collect();
         if !self.buf.is_empty() {
-            return self.mathdivider()
-        }
-        else {
+            return self.mathdivider();
+        } else {
             self.invalid_equation(Error::msg("Invalid equation (Empty equation)"), "-".into());
             0.0
         }
     }
-    
+
     fn mathdivider(mut self) -> f64 {
         for item in self.buf.clone() {
             match Engine::mathdecide(&item) {
                 Ok(ok) => self.num_list.push(ok),
-                Err(_expr) => self.expr_list.push(
-                    match item.as_str() {
-                        "+" => Math::Addition,
-                        "-" => Math::Subtraction,
-                        "*" => Math::Multiplication,
-                        "/" | "%" | ":" => Math::Divide,
-                        "^" | "pow" => Math::Power,         
-                        _ => {/*Go apeshit*/ self.invalid_equation(Error::msg("Syntax Error (Invalid expression)"), item.clone()); Math::KurvaAnyad},               
+                Err(_expr) => self.expr_list.push(match item.as_str() {
+                    "+" => Math::Addition,
+                    "-" => Math::Subtraction,
+                    "*" => Math::Multiplication,
+                    "/" | "%" | ":" => Math::Divide,
+                    "^" | "pow" => Math::Power,
+                    _ => {
+                        /*Go apeshit*/
+                        self.invalid_equation(
+                            Error::msg("Syntax Error (Invalid expression)"),
+                            item.clone(),
+                        );
+                        Math::KurvaAnyad
                     }
-                ),
+                }),
             }
         }
         //finsihed sorting the 2 vectors
@@ -91,7 +113,7 @@ impl Engine {
     fn mathengine(&mut self) -> f64 {
         let mut len: usize = self.expr_list.len();
         let mut index = 0;
-        
+
         while index < len {
             if self.expr_list[index] == Math::Multiplication {
                 let result = self.multiplication(self.num_list[index], self.num_list[index + 1]);
@@ -100,7 +122,6 @@ impl Engine {
                 self.num_list.remove(index);
                 self.num_list.insert(index, result);
                 len -= 1; // Decrement the length since we removed an element
-            
             } else if self.expr_list[index] == Math::Divide {
                 let result = self.divide(self.num_list[index], self.num_list[index + 1]);
                 self.expr_list.remove(index);
@@ -115,8 +136,7 @@ impl Engine {
                 self.num_list.remove(index);
                 self.num_list.insert(index, result);
                 len -= 1; // Decrement the length since we removed an element
-            }
-            else {
+            } else {
                 index += 1;
             }
         }
@@ -136,34 +156,37 @@ impl Engine {
                 self.num_list.remove(index);
                 self.num_list.insert(index, result);
                 len -= 1; // Decrement the length since we removed an element
-            }
-            else {
+            } else {
                 index += 1;
             }
-
-
         }
-        
+
         if self.num_list.len() > 1 {
-            self.invalid_equation(Error::msg("Syntax Error (Invalid equation)"), self.num_list.iter().map(|f| format!("{} ", f).to_string()).collect());
+            self.invalid_equation(
+                Error::msg("Syntax Error (Invalid equation)"),
+                self.num_list
+                    .iter()
+                    .map(|f| format!("{} ", f).to_string())
+                    .collect(),
+            );
         }
 
         self.num_list[0]
     }
 
-    fn multiplication(&self, num1 : f64, num2: f64) -> f64 {
+    fn multiplication(&self, num1: f64, num2: f64) -> f64 {
         return num1 * num2;
     }
-    fn divide(&self, num1 : f64, num2: f64) -> f64 {
+    fn divide(&self, num1: f64, num2: f64) -> f64 {
         return num1 / num2;
     }
-    fn addition(&self, num1 : f64, num2: f64) -> f64 {
+    fn addition(&self, num1: f64, num2: f64) -> f64 {
         return num1 + num2;
     }
-    fn subtraction(&self, num1 : f64, num2: f64) -> f64 {
+    fn subtraction(&self, num1: f64, num2: f64) -> f64 {
         return num1 - num2;
     }
-    fn power(&self, num1 : f64, num2: f64) -> f64 {
+    fn power(&self, num1: f64, num2: f64) -> f64 {
         return num1.powf(num2);
     }
 }
